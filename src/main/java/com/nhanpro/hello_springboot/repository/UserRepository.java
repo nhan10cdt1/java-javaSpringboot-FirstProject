dc9584c10ab4ddfd13593abd3c5ac7e5a17fb33d
nhan10cdt1 <nhanbkdn1211@gmail.com>
2025-02-27 09:42:35 +0900

    #2 Kết nối Database với Spring JPA và tạo API CRUD

---
+++ src/main/java/com/nhanpro/hello_springboot/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.nhanpro.hello_springboot.repository;
+
+import com.nhanpro.hello_springboot.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+}
---
+++ src/main/java/com/nhanpro/hello_springboot/service/UserService.java
@@ -0,0 +1,52 @@
+package com.nhanpro.hello_springboot.service;
+
+import com.nhanpro.hello_springboot.dto.request.UserCreationRequest;
+import com.nhanpro.hello_springboot.dto.request.UserUpdateRequset;
+import com.nhanpro.hello_springboot.entity.User;
+import com.nhanpro.hello_springboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(UserCreationRequest request){
+        User user = new User();
+        user.setUserName (request.getUserName());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+
+    }
+
+    public User updateUser(String userId, UserUpdateRequset request){
+        User user = getUser(userId);
+
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return  userRepository.save(user);
+    }
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+    public User getUser(String userId){
+        return userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+
+
+    }
+
+    public void deleteUser(String userId){
+        userRepository.deleteById(userId);
+    }
+}
